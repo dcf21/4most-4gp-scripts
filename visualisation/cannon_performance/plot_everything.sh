11aa676c0f5fbf0a7b713593ad2072815d9a81d6
Dominic Ford <dcf21-github@dcford.org.uk>
2017-10-07 18:49:44 +0100

    Fixed filenames in example plot script

--- visualisation/cannon_performance/plot_everything.sh
+++ visualisation/cannon_performance/plot_everything.sh
@@ -2,27 +2,27 @@
 
 source ../../../virtualenv/bin/activate
 
-python scatter_plot_arrows.py --output-stub ../../output_plots/apogee_teff_logg_hrs_offset_arrows \
+python scatter_plot_arrows.py --output-stub ../../output_plots/apokasc_teff_logg_hrs_offset_arrows \
                               --label "Teff{5100:4000}" --label "logg{3.8:1.2}" \
                               --label-axis-latex "Teff" --label-axis-latex "log(g)" \
-                              --cannon_output ../../output_data/cannon_4fs_hrs.dat
+                              --cannon_output ../../output_data/cannon_apokasc_hrs.json
 
-python scatter_plot_arrows.py --output-stub ../../output_plots/apogee_teff_logg_lrs_offset_arrows \
+python scatter_plot_arrows.py --output-stub ../../output_plots/apokasc_teff_logg_lrs_offset_arrows \
                               --label "Teff{5100:4000}" --label "logg{3.8:1.2}" \
                               --label-axis-latex "Teff" --label-axis-latex "log(g)" \
-                              --cannon_output ../../output_data/cannon_4fs_lrs.dat
+                              --cannon_output ../../output_data/cannon_apokasc_lrs.json
 
 python mean_performance_vs_snr.py \
-  --cannon-output ../../output_data/cannon_hawkins_lrs.dat --dataset-label "Hawkins LRS" \
-  --cannon-output ../../output_data/cannon_hawkins_hrs.dat --dataset-label "Hawkins HRS" \
-  --cannon-output ../../output_data/cannon_4fs_lrs.dat --dataset-label "Ford LRS" \
-  --cannon-output ../../output_data/cannon_4fs_hrs.dat --dataset-label "Ford HRS" \
-  --output-file ../../output_plots/apogee_mean_performance
+  --cannon-output ../../output_data/cannon_hawkins_lrs.json --dataset-label "Hawkins LRS" \
+  --cannon-output ../../output_data/cannon_hawkins_hrs.json --dataset-label "Hawkins HRS" \
+  --cannon-output ../../output_data/cannon_apokasc_lrs.json --dataset-label "Ford LRS" \
+  --cannon-output ../../output_data/cannon_apokasc_hrs.json --dataset-label "Ford HRS" \
+  --output-file ../../output_plots/apokasc_mean_performance
 
 python scatter_plot_coloured.py --label "Teff{5100:4000}" --label "logg{3.8:1.2}" \
                                 --label-axis-latex "Teff" --label-axis-latex "log(g)" --label-axis-latex "Error in Teff" \
                                 --colour-by-label "Teff{:}" \
                                 --colour-range-min -400 --colour-range-max 400 \
-                                --cannon_output ../../output_data/cannon_4fs_lrs.dat \
-                                --output-stub ../../output_plots/apogee_Teff_performance_hr
+                                --cannon_output ../../output_data/cannon_apokasc_lrs.json \
+                                --output-stub ../../output_plots/apokasc_Teff_performance_hr
 
