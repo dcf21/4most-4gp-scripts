4daf10ccfcf635055d0e96e26383f7f023cc9f72
Dominic Ford <dcf21-git@dcford.org.uk>
2018-02-04 18:00:09 +0100

    Added cn tests to example script

--- test_cannon/examples.sh
+++ test_cannon/examples.sh
@@ -5,6 +5,25 @@ source ../../virtualenv/bin/activate
 
 mkdir -p ../output_data/cannon
 
+python2.7 cannon_test.py --train "4fs_ahm2017_sample_hrs[SNR=250]" \
+                         --test "4fs_ahm2017_perturbed_hrs" \
+                         --continuum-normalisation "running_mean" \
+                         --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+                         --description "4MOST HRS (censored) - 10 labels - Train on GES UVES AHM2017. Test on perturbed version." \
+                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
+                         --assume-scaled-solar \
+                         --output-file "../output_data/cannon/cannon_ahm2017_perturbed_censored_hrs_cn_10label"
+python2.7 cannon_test.py --train "4fs_ahm2017_sample_lrs[SNR=250]" \
+                         --test "4fs_ahm2017_perturbed_lrs" \
+                         --continuum-normalisation "running_mean" \
+                         --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+                         --description "4MOST LRS (censored) - 10 labels - Train on GES UVES AHM2017. Test on perturbed version." \
+                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
+                         --assume-scaled-solar \
+                         --output-file "../output_data/cannon/cannon_ahm2017_perturbed_censored_lrs_cn_10label"
+
+# ----------------------
+
 python2.7 cannon_test.py --train "4fs_ahm2017_sample_lrs[SNR=250]" \
                          --test "4fs_demo_stars_lrs" \
                          --description "4MOST LRS - 3 labels - Quick APOKASC test." \
