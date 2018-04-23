b53fedc4b7a5f2d7c00f3e8411a0e271a111200e
Dominic Ford <dcf21-git@dcford.org.uk>
2018-04-23 16:38:47 +0200

    added plotting code

--- visualisation/exposure_times/mean_exposure_vs_snr.py
+++ visualisation/exposure_times/mean_exposure_vs_snr.py
@@ -6,7 +6,9 @@ Take a SpectrumLibrary and tabulate the average exposure time at each SNR within
 """
 
 import argparse
+import os
 from os import path as os_path
+import pwd
 import numpy as np
 
 from fourgp_speclib import SpectrumLibrarySqlite
@@ -15,6 +17,8 @@ from fourgp_speclib import SpectrumLibrarySqlite
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--library', required=True, action="append", dest='libraries',
                     help="Library of spectra we should output stellar labels for.")
+parser.add_argument('--wavelength', required=True, dest='wavelengths',
+                    help="Wavelength to use for converting SNR/pixel to SNR/A")
 parser.add_argument('--output-file', default="/tmp/exposure_times_vs_snr.dat", dest='output_file',
                     help="Data file to write output to.")
 args = parser.parse_args()
@@ -25,6 +29,11 @@ workspace = os_path.join(our_path, "..", "..", "workspace")
 
 # Construct list of exposure times at each SNR
 exposures_by_snr = {}
+
+# Raster on which data is sampled
+raster = None
+raster_diff = None
+pixels_per_angstrom = None
 
 # Open output data file
 with open(args.output_file, "w") as output:
@@ -38,6 +47,17 @@ with open(args.output_file, "w") as output:
 
         # Loop over objects in SpectrumLibrary
         for item in library_object.search(**constraints):
+
+            # Convert SNR/pixel to SNR/A
+            if raster is None:
+                if args.wavelength > 0:
+                    spectrum = library_object.open(ids=item['specId']).extract_item(0)
+                    raster = spectrum.wavelengths
+                    raster_diff = np.diff(raster[raster > args.wavelength])
+                    pixels_per_angstrom = 1.0 / raster_diff[0]
+                else:
+                    pixels_per_angstrom = 1
+
             metadata = library_object.get_metadata(ids=item['specId'])[0]
 
             exposure = metadata['exposure']
@@ -55,7 +75,57 @@ unique_snrs_sorted.sort()
 
 # Write output data file
 with open(args.output_file,"w") as f:
-    f.write("# SNR/pixel Exposure time\n")
+    f.write("# SNR/pixel  SNR/A  Mean_exposure_time  StdDev_of_exposure_times\n")
     for snr in unique_snrs_sorted:
-        f.write("{0:.3f}  {1:.3f}\n".format(snr, np.mean(exposures_by_snr[snr])))
+        f.write("{0:.3f}  {1:.3f}  {2:.3f}  {3:.3f}\n".format(snr, snr * np.sqrt(pixels_per_angstrom),
+                                                              np.mean(exposures_by_snr[snr]),
+                                                              np.std(exposures_by_snr[snr])))
 
+# Produce plot of exposure time versus SNR
+stem = args.output_file
+aspect = 1 / 1.618034  # Golden ratio
+plot_width = 18
+date_stamp = True
+
+user_name = pwd.getpwuid( os.getuid() ).pw_gecos.split(",")[0]
+plot_creator = "{}, {}".format(user_name, time.strftime("%d %b %Y"))
+
+with open("{}.ppl".format(stem), "w") as ppl:
+    ppl.write("""
+    
+    set width {0}
+    set size ratio {1}
+    set term dpi 200
+    set nodisplay ; set multiplot
+    set label 1 texify("{2} ({3} stars)") page 1, page {4}
+    
+    """.format(plot_width, aspect,
+               args.libraries[0],
+               len(exposures_by_snr[unique_snrs_sorted[0]]),
+               plot_width * aspect - 0.8))
+
+    if date_stamp:
+        ppl.write("""
+        set fontsize 0.8
+        text "{}" at 0, {}
+        """.format(plot_creator, plot_width * aspect + 0.4))
+
+    ppl.write("set fontsize 1.3\n")  # 1.6
+    ppl.write("set key top right ; set keycols 2\n")
+    ppl.write("set ylabel \"Exposure time [min]\"\n")
+    ppl.write("set xlabel \"SNR/$\AA$\"\n")
+
+    # Set axis limits
+    ppl.write("set yrange [1:600] ; set log y\n")
+    ppl.write("set xrange [0:250]\n")
+
+    ppl.write("plot '{}' using yerrorbars using 2:3:4\n".format(args.output_file))
+
+    ppl.write("""
+    
+    set term eps ; set output '{0}.eps' ; set display ; refresh
+    set term png ; set output '{0}.png' ; set display ; refresh
+    set term pdf ; set output '{0}.pdf' ; set display ; refresh
+
+    """.format(stem))
+os.system("pyxplot {}.ppl".format(stem))
