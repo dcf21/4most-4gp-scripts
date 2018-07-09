1105a8f99fe9284abfe57dcebd4819b5493cbade
Dominic Ford <dcf21-git@dcford.org.uk>
2018-07-09 18:14:54 +0200

    More hacking of mean_performance_vs_label.py up into pieces

--- src/helper_code/abscissa_information.py
+++ src/helper_code/abscissa_information.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """
-All of the horizontal axes we can plot precision against. The parameter "abscissa_label" should be one of the
-keys to this dictionary.
+Metadata data about all of the horizontal axes that we can plot precision against. When invoking any of the
+<offset_*.py> scripts, the argument "abscissa_label" should be one of the keys to this dictionary.
 """
 
 
 class AbcissaInformation:
+    """
+    Metadata data about all of the horizontal axes that we can plot precision against. When invoking any of the
+    <offset_*.py> scripts, the argument "abscissa_label" should be one of the keys to this dictionary.
+    """
+
     def __init__(self):
         self.abscissa_labels = {
             # label name, latex label, log axes, axis range
-            "SNR/A": ["SNR", "$S/N$ $[{\\rm \\AA}^{-1}]$", False, (0, 250)],
-            "SNR/pixel": ["SNR", "$S/N$ $[{\\rm pixel}^{-1}]$", False, (0, 250)],
-            "ebv": ["e_bv", "$E(B-V)$", True, (0.04, 3)],
-            "rv": ["rv", "RV [m/s]", True, (800, 50e3)]
+            "SNR/A": {"field": "SNR",
+                      "latex": "$S/N$ $[{\\rm \\AA}^{-1}]$",
+                      "log_axis": False,
+                      "axis_range": (0, 250)},
+            "SNR/pixel": {"field": "SNR",
+                          "latex": "$S/N$ $[{\\rm pixel}^{-1}]$",
+                          "log_axis": False,
+                          "axis_range": (0, 250)},
+            "ebv": {"field": "e_bv",
+                    "latex": "$E(B-V)$",
+                    "log_axis": True,
+                    "axis_range": (0.04, 3)},
+            "rv": {"field": "rv",
+                   "latex": "RV [m/s]",
+                   "log_axis": True,
+                   "axis_range": (800, 50e3)}
         }
