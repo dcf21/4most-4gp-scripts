b9f34dd7e350e094fc36cd23ece47eeb57eb9d82
Dominic Ford <dcf21-git@dcford.org.uk>
2018-07-12 19:24:01 +0200

    added comments to code

--- src/scripts/import_spectra/import_apokasc.py
+++ src/scripts/import_spectra/import_apokasc.py
@@ -7,11 +7,11 @@
 # <python import_apokasc.py>, but <./import_apokasc.py> will not work.
 
 """
-Take the APOKASC training set and test sets, and turn them into SpectrumLibraries for use in 4MOST 4GP
+Take the APOKASC training set and test sets, as defined in ASCII tables provided by Keith Hawkins, and turn them into
+SpectrumLibraries for use in 4MOST 4GP
 """
 
 import os
-import re
 import glob
 import argparse
 from os import path as os_path
@@ -22,14 +22,14 @@ from fourgp_speclib import SpectrumLibrarySqlite, Spectrum
 
 # Path to where we find Keith Hawkins's <4MOST_testspectra>
 our_path = os_path.split(os_path.abspath(__file__))[0]
-test_spectra_path = os_path.join(our_path, "../../4MOST_testspectra")
+test_spectra_path = os_path.join(our_path, "../../../../4MOST_testspectra")
 
 # Set path to workspace where we create libraries of spectra
 workspace = os_path.join(our_path, "../../../workspace")
 os.system("mkdir -p {}".format(workspace))
 
 
-# Convenience function, coz it would've been too helpful for astropy to actually provide dictionary access to rows
+# Convenience function to provide dictionary access to rows of an astropy table
 def astropy_row_to_dict(x):
     return dict([(i, x[i]) for i in x.columns])
 
@@ -40,24 +40,26 @@ parser.add_argument('--training-set',
                     required=False,
                     default=os_path.join(test_spectra_path, "trainingset_param.tab"),
                     dest="training_set",
-                    help="Specify an ASCII table which lists the stellar parameters of the training stars.")
+                    help="Specify the filename of the ASCII table which lists the stellar parameters of the training "
+                         "stars.")
 parser.add_argument('--test-set',
                     required=False,
                     default=os_path.join(test_spectra_path, "testset_param.tab"),
                     dest="test_set",
-                    help="Specify an ASCII table which lists the stellar parameters of the test stars.")
+                    help="Specify the filename of the ASCII table which lists the stellar parameters of the test "
+                         "stars.")
 args = parser.parse_args()
 
 # Start logger
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s:%(filename)s:%(message)s',
                     datefmt='%d/%m/%Y %H:%M:%S')
-logger = logging.getLogger(__name__)
-logger.info("Importing APOKASC grid of spectra")
+logger = logging.getLogger(__name__.strip())
+logger.info("Importing APOKASC sample of spectra")
 
 # Table supplies list of stars in the APOKASC training set, giving the stellar labels for each star in the training set
 training_set = Table.read(args.training_set, format="ascii")
 
-# Table supplies list of expected stellar labels for each star in the test data set
+# Table supplies list of stellar labels for each star in the test set
 expected_test_output = Table.read(args.test_set, format="ascii")
 expected_test_output_dict = dict([(star['Starname'], astropy_row_to_dict(star)) for star in expected_test_output])
 
