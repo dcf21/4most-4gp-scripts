272c95754e22d021d67255cd2bba8b02c13a2b42
Dominic Ford <dcf21@vesta.astro.lu.se>
2018-09-05 14:32:25 +0200

    implemented script to do instrumental profile test

--- src/scripts/test_cannon/examples/examples_galah_20180830.sh
+++ src/scripts/test_cannon/examples/examples_galah_20180830.sh
@@ -14,32 +14,22 @@ mkdir -p ../../../output_data/cannon
 
 # -----------------------------------------------------------------------------------------
 
-python2.7 cannon_test.py --train "galah_training_sample_4fs_hrs[SNR=250]" \
-                         --test "galah_test_sample_4fs_he_hrs_50only" \
-                         --description "4MOST HRS (with half-ellipse convolution) - 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
-                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
-                         --assume-scaled-solar \
-                         --output-file "../../../output_data/cannon/cannon_galah_he_hrs_10label"
-python2.7 cannon_test.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
-                         --test "galah_test_sample_4fs_he_lrs_50only" \
-                         --description "4MOST LRS (with half-ellipse convolution) - 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
-                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
-                         --assume-scaled-solar \
-                         --output-file "../../../output_data/cannon/cannon_galah_he_lrs_10label"
+for mode in hrs lrs
+do
 
-python2.7 cannon_test.py --train "galah_training_sample_4fs_hrs[SNR=250]" \
-                         --test "galah_test_sample_4fs_he_hrs_50only" \
-                         --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
-                         --description "4MOST HRS (with half-ellipse convolution; censored) - 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
-                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
-                         --assume-scaled-solar \
-                         --output-file "../../../output_data/cannon/cannon_galah_he_censored_hrs_10label"
-python2.7 cannon_test.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
-                         --test "galah_test_sample_4fs_he_lrs_50only" \
-                         --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
-                         --description "4MOST LRS (with half-ellipse convolution; censored) - 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
-                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
-                         --assume-scaled-solar \
-                         --output-file "../../../output_data/cannon/cannon_galah_he_censored_lrs_10label"
+for he_width in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2
+do
 
+
+python2.7 cannon_test.py --train "galah_training_sample_4fs_he1.7_${mode}" \
+                         --test "galah_test_sample_4fs_he${he_width}_${mode}" \
+                         --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+                         --description "4MOST HRS (with ${he_width}-pixel half-ellipse convolution; censored) - 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
+                         --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
+                         --assume-scaled-solar \
+                         --output-file "../../../output_data/cannon/cannon_galah_he${he_width}_censored_${mode}_10label"
+
+done
+
+done
 
